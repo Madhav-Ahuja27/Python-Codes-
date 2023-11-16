db028fcdf6c7529bd2abf178efd632820534c33b
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2023-11-16 14:06:02 +0530

    Create cwh_day1.py

---
+++ cwh_day1.py
@@ -0,0 +1,39 @@
+# def func():
+#     try:
+#         inp=int(input())
+#         return inp
+#     except:
+#         print("Skill issue")
+#         return 1
+#     finally:
+#         print("sure")
+
+    
+# x=func()
+# print(x)
+# r=int(input())
+# if r<0 or r>5:
+#     raise ValueError("Bruh you noob")
+# else:
+#     print(r)
+
+# li=list(range(1,6))
+# for enu,i in enumerate(li,start=1):
+#     if enu==3:
+#         print("here")
+#     else:
+#         print(i)
+
+# r=int(input())
+# print(r) if r>0 and r<10 else print("negative") if r<0 else print("positive")
+
+#conda is a package manager used in data science and scientific computing and is used for managing 
+
+try:
+    print("this")
+except:
+    print("that")
+finally:
+    print("always")
+
+print("always executed")
