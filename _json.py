1e45f8822cb26b3c716b1c17944b4fad74c70c7b
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2023-11-16 14:20:18 +0530

    Update _json.py

--- _json.py
+++ _json.py
@@ -20,3 +20,73 @@ opener=open("new.json")
 data=json.load(opener)
 for i in data:
     print(i,data[i])
+
+# '''JavaScripts Object Notation
+# Key value pairs(dicts) [list of]'''
+#but json takes list of dictionaries
+# import json
+# data={'name':'madhav','year':1,'grp':'B'}
+# data2={'name':'madhav2','year':2,'grp':'A'}
+# data3={'name':'madhav3','year':3,'grp':'C'}
+# with open('file.json','w') as file:
+#   json.dump([data,data2,data3],file, indent=4,separators=("; "," is equal to "),sort_keys=False)
+
+# with open('file.json') as file:
+#   json.loads(file)
+
+# dict={}
+# def dictGet(inp):
+  
+  
+#   while inp!="n":
+#     dictGet(inp)
+
+
+# inp=input("Enter y/n")
+# if inp=="y":
+#   dictGet(inp)
+
+
+
+# def Input(inp,dict):
+#   if inp!="n" and inp=="y":
+#     dict2={}
+#     name=input("enter name:")
+#     clas=input("enter class:")
+#     grp=input("enter group:")
+#     dict2['name']=name
+#     dict2['class']=clas
+#     dict2['group']=grp
+#     dict=(dict | dict2)
+#     print("Values added successfully")
+#     Input(input("Enter y/n:"),dict)
+    
+# dict={}
+# Input(input("Enter y/n:"),dict)
+# print(dict)
+
+
+# dict1={'name':'madhav','year':1,'grp':'B'}
+# dict2={'name':'mxyv2','year':2,'grp':'A'}
+# print(dict1 | dict2)
+
+def Input(inp,lst):
+  if inp!="n" and inp=="y":
+    dict={}
+    name=input("enter name:")
+    clas=input("enter class:")
+    grp=input("enter group:")
+    dict['name']=name
+    dict['class']=clas
+    dict['group']=grp
+    lst.append(dict)
+    print("Values added successfully")
+    Input(input("Enter y/n:"),lst)
+
+lst=[]
+
+Input(input("Enter y/n:"),lst)
+print(lst)
+import json
+with open('file.json','w') as file:
+  json.dump(lst,file, indent=4,separators=("; "," is equal to "),sort_keys=False)
