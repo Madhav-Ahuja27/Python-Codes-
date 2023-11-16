13d55d85a3c3216a21f8023fccde8bb376d2414b
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2023-11-16 14:05:32 +0530

    Create time.py

---
+++ time.py
@@ -0,0 +1,26 @@
+# import time
+# print(t:=time.time())
+
+# t1=time.time()
+# for i in range(10):
+#     print(i)
+# t2=time.time()
+# i=0
+# while i<5:
+#     print(i)
+#     i+=1
+# t3=time.time()
+
+# print("for loop",t2-t1)
+# print("while loop",t3-t1)
+
+print(a:="Flase")
+# list1=list()
+# while (i:=input("Enter bro"))!='quit':
+#     list1.append(i)
+# print(list1)
+
+list1 = []
+while (i := input("Enter bro").strip()) != 'quit':
+    list1.append(i)
+print(list1)
