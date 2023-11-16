eadf50ae4f41f9f640270dd6eb2c1ca1bbff4af8
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2023-11-16 14:04:45 +0530

    Create reverse_each_word.py

---
+++ reverse_each_word.py
@@ -0,0 +1,36 @@
+# from sys import stdin
+# def reverseEachWord(string) :
+#     string.split()
+#     newstr=""
+#     for i in string:
+#         j=i[::-1]   
+#         newstr+=j
+#     return newstr
+#     #Your code goes here.
+
+# sizea=int(input())
+# N=[int (i) for i in input().split()]
+# x=int(input())
+# for i in range(sizea+1):
+#     if x==i:
+#         print(N.index(i))
+#         break
+# else:
+#     print(-1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
