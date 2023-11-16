715c0ed9c5359040e315c965af936e2d7992ce7c
Madhav-Ahuja27 <150770153+Madhav-Ahuja27@users.noreply.github.com>
2023-11-16 14:05:01 +0530

    Create cmath.py

---
+++ cmath.py
@@ -0,0 +1,20 @@
+#used for complex numbers/data type and always returns complex numbers
+import cmath
+import math
+# print(cmath.sqrt(4))
+# print(cmath.log(5j))
+# print(cmath.exp(5j))
+# print(cmath.acos(60))
+# print(cmath.phase(5+23j))
+# print(cmath.polar(5+23j))
+
+# print(pow(5,2))
+# print(math.ceil(6.69))
+# print(math.fabs(-6.75757))
+# print(math.floor(56.54))
+
+# print(math.e)
+# print(math.pi)
+
+# print(dir())
+
